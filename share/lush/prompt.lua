34a079c7db633f61d50bfd6a0e7d1a56c3259db0
Jesse Weaver <pianohacker@gmail.com>
2014-01-22 14:08:06 -0700

    Add layer of indirection for bindings, fix Mac issue

--- share/lush/prompt.lua
+++ share/lush/prompt.lua
@@ -38,129 +38,7 @@ function Editor:new(sh)
 	return new_obj
 end
 
-function Editor.actions:move_left()
-	-- Handle left arrow key
-	if line.position == 1 then return end
-
-	line.position = line.position - 1
-	line:move_cur()
-end
-
-function Editor:bind_defaults()
-	k = lush.term.tigetstr
-
-	self:bind(k('kbs'), function(line)
-		if line.position == 1 then return end
-		
-		line.content = line.content:sub(1, line.position - 2) .. line.content:sub(line.position)
-		line.position = line.position - 1
-		line:refresh()
-	end)
-
-	self:bind(k('cr'), self.key_enter)
-	self:bind('\n', self.key_enter)
-
-	-- The gsub is because terminfo is a _filthy_ liar
-	-- More specifically, the termcap for xterm only contains the key sequences
-	-- for the numpad versions of the arrow keys.
-	-- Why? Because your mother dresses you funny, that's why.
-	self:bind(k('kcub1'):gsub('O', '['), function(line)
-		-- Handle left arrow key
-		if line.position == 1 then return end
-
-		line.position = line.position - 1
-		line:move_cur()
-	end)
-
-	self:bind(k('kcuf1'):gsub('O', '['), function(line)
-		-- Handle right arrow key
-		if line.position == #line.content + 1 then return end
-
-		line.position = line.position + 1
-		line:move_cur()
-	end)
-
-	-- Map the up and down arrow keys to null handlers
-	self:bind(k('kcuu1'):gsub('O', '['), function(line)
-		if line.history_pos == 1 then return end
-
-		line:switch_history(line.history_pos - 1)
-	end)
-
-	self:bind(k('kcud1'):gsub('O', '['), function(line)
-		if line.history_pos == #line.act_history then return end
-
-		line:switch_history(line.history_pos + 1)
-	end)
-
-	self:bind(k('khome'), function(line)
-		line.position = 1
-		line:move_cur()
-	end)
-
-	self:bind(k('kend'), function(line)
-		line.position = #line.content + 1
-		line:move_cur()
-	end)
-
-	self:bind(k('ht'), function(line)
-		line:complete()
-	end)
-end
-
-function Editor:bind(seq, func)
-	self.handlers[seq] = func
-end
-
--- Checks to see if the current sequence is the prefix of a handled string
--- Useful for seeing if we should wait for more characters or just output the sequence
-function Editor:handler_prefix(seq)
-	for k, v in pairs(self.handlers) do
-		if k:sub(1, #seq) == seq then return true end
-	end
-
-	return false
-end
-
-function Editor:key_enter()
-	self.ready = true
-	self:move_cur(#self.content + 1)
-end
-
--- Resets the state of the line editor
-function Editor:reset()
-	self.content = ''
-	self.position = 1
-	self.ready = false
-end
-
--- Move the cursor to the following
-function Editor:move_cur(position)
-	lush.term.putcap('hpa', self.start_column - 1 + (position or self.position) - 1)
-end
-
--- Clear the screen from the current cursor position to the end of the line
-function Editor:clear_to_end()
-	lush.term.putcap('el')
-end
-
--- Output the current content of the line, with the cursor ending up in the right position
-function Editor:refresh()
-	self:move_cur(1)
-	self:clear_to_end()
-	io.write(self.content)
-	self:move_cur()
-end
-
-function Editor:switch_history(new_pos)
-	self.act_history[self.history_pos] = self.content
-	self.history_pos = new_pos
-	self.content = self.act_history[self.history_pos]
-	self.position = #self.content + 1
-	self:refresh()
-end
-
-function Editor:complete()
+function Editor.actions:complete()
 	local prev_space = self.position
 	while prev_space > 0 and self.content:byte(prev_space) ~= 32 do
 		prev_space = prev_space - 1
@@ -199,6 +77,139 @@ function Editor:complete()
 	self:refresh()
 end
 
+function Editor.actions:delete_left()
+	if self.position == 1 then return end
+	
+	self.content = self.content:sub(1, self.position - 2) .. self.content:sub(self.position)
+	self.position = self.position - 1
+	self:refresh()
+end
+
+function Editor.actions:delete_right()
+	self.content = self.content:sub(1, self.position - 1) .. self.content:sub(self.position + 1)
+	self:refresh()
+end
+
+function Editor.actions:finish()
+	self.ready = true
+	self:move_cur(#self.content + 1)
+end
+
+function Editor.actions:history_show_prev()
+	if self.history_pos == 1 then return end
+
+	self:switch_history(self.history_pos - 1)
+end
+
+function Editor.actions:history_show_next()
+	if self.history_pos == #self.act_history then return end
+
+	self:switch_history(self.history_pos + 1)
+end
+
+function Editor.actions:move_left()
+	-- Handle left arrow key
+	if self.position == 1 then return end
+
+	self.position = self.position - 1
+	self:move_cur()
+end
+
+function Editor.actions:move_right()
+	-- Handle right arrow key
+	if self.position == #self.content + 1 then return end
+
+	self.position = self.position + 1
+	self:move_cur()
+end
+
+function Editor.actions:move_to_start()
+	self.position = 1
+	self:move_cur()
+end
+
+function Editor.actions:move_to_end()
+	self.position = #self.content + 1
+	self:move_cur()
+end
+
+function Editor:bind_defaults()
+	k = lush.term.tigetstr
+
+	self:bind(k('kbs'), 'delete_left')
+	self:bind('\b', 'delete_left')
+	self:bind(k('kdch1'), 'delete_right')
+
+	self:bind(k('cr'), 'finish')
+	self:bind('\n', 'finish')
+
+	-- The gsub is because terminfo is a _filthy_ liar
+	-- More specifically, the termcap for xterm only contains the key sequences
+	-- for the numpad versions of the arrow keys.
+	-- Why? Because your mother dresses you funny, that's why.
+	self:bind(k('kcub1'):gsub('O', '['), 'move_left') -- Left arrow key
+	self:bind(k('kcuf1'):gsub('O', '['), 'move_right') -- Right arrow key
+	self:bind(k('kcuu1'):gsub('O', '['), 'history_show_prev') -- Up arrow key
+	self:bind(k('kcud1'):gsub('O', '['), 'history_show_next') -- Down arrow key
+
+	self:bind(k('khome'), 'move_to_start')
+	self:bind(k('kend'), 'move_to_end')
+	self:bind(k('ht'), 'complete')
+end
+
+function Editor:bind(seq, func)
+	if #seq == 0 then return end
+
+	if type(func) == 'string' then
+		self.handlers[seq] = self.actions[func]
+	else
+		self.handlers[seq] = func
+	end
+end
+
+-- Checks to see if the current sequence is the prefix of a handled string
+-- Useful for seeing if we should wait for more characters or just output the sequence
+function Editor:handler_prefix(seq)
+	for k, v in pairs(self.handlers) do
+		if k:sub(1, #seq) == seq then return true end
+	end
+
+	return false
+end
+
+-- Resets the state of the line editor
+function Editor:reset()
+	self.content = ''
+	self.position = 1
+	self.ready = false
+end
+
+-- Move the cursor to the following
+function Editor:move_cur(position)
+	lush.term.putcap('hpa', self.start_column - 1 + (position or self.position) - 1)
+end
+
+-- Clear the screen from the current cursor position to the end of the line
+function Editor:clear_to_end()
+	lush.term.putcap('el')
+end
+
+-- Output the current content of the line, with the cursor ending up in the right position
+function Editor:refresh()
+	self:move_cur(1)
+	self:clear_to_end()
+	io.write(self.content)
+	self:move_cur()
+end
+
+function Editor:switch_history(new_pos)
+	self.act_history[self.history_pos] = self.content
+	self.history_pos = new_pos
+	self.content = self.act_history[self.history_pos]
+	self.position = #self.content + 1
+	self:refresh()
+end
+
 function Editor:getline(start_column)
 	self.act_history = {unpack(self.history)}
 	self.act_history[#self.history + 1] = ''
@@ -217,7 +228,7 @@ function Editor:getline(start_column)
 			self.handlers[seq](self)
 			seq = ''
 		elseif not self:handler_prefix(seq) then
-			if #seq > 1 then log.internal('Unrecognized sequence: %s', dumpstr(seq)) end
+			if #seq > 1 or #seq.match('^%c$') then log.internal('Unrecognized sequence: %s', dumpstr(seq)) end
 			seq = seq:gsub('%c', '')
 			self.content = self.content:sub(1, self.position - 1) .. seq .. self.content:sub(self.position)
 			self.position = self.position + #seq
